b4dce472e3c0482595184b12628084206d5e33b2
Martin Wragg <mdwragg@gmail.com>
2017-03-21 16:10:41 -0700

    Add auto-dismiss test.

--- test/px-alert-message-custom-tests.js
+++ test/px-alert-message-custom-tests.js
@@ -1,34 +1,28 @@
 describe('Alert action buttons update correctly', function() {
   //Button displays
-    // it('action button reflects the "action" property  open', function(done) {
-    //   Polymer.Base.async(function(){
-    //     var btn = autoDismissAlert.querySelector('.action button');
-    //     assert.equal(btn.textContent, "Open");
-    //     done();
-    //   }, 500);
-    //
-    // });
-    //
-    // it('action button reflects the "action" property  ok', function() {
-    //   var btn = okBtnAlert.querySelector('.action button');
-    //   assert.equal(okBtnAlert.resources.en.OK, "OK");
-    // });
-    //
+    it('action button reflects the "action" property  open', function(done) {
+      var autoDismissAlertEl = fixture('PxDismissAlertMessage');
+      flush(function(){
+        var btn = autoDismissAlertEl.querySelector('.action button');
+        assert.equal(btn.textContent, "Open");
+        done();
+      });
+    });
 
     it('action section does not exist', function() {
-        var alertMessageEl = fixture('PxBasicAlertMessage');
-        var actionBtn = alertMessageEl.querySelector('.action');
-        expect(actionBtn).to.not.exist;
+      var alertMessageEl = fixture('PxBasicAlertMessage');
+      var actionBtn = alertMessageEl.querySelector('.action');
+      expect(actionBtn).to.not.exist;
     });
 
     it('action button reflects the "dismiss" property', function(done) {
-        var alertMessageEl = fixture('PxBasicAlertMessage');
-        alertMessageEl.action = "dismiss";
-        flush(function(){
-          var icon = alertMessageEl.querySelector('.action .dismiss > iron-icon');
-          expect(icon).to.exist;
-          done();
-        });
+      var alertMessageEl = fixture('PxBasicAlertMessage');
+      alertMessageEl.action = "dismiss";
+      flush(function(){
+        var icon = alertMessageEl.querySelector('.action .dismiss > iron-icon');
+        expect(icon).to.exist;
+        done();
+      });
     });
 
     it('action button reflects the "action" property  ok', function() {
@@ -37,6 +31,7 @@ describe('Alert action buttons update correctly', function() {
       flush(function(){
         var btn = alertMessageEl.querySelector('.action button');
         assert.equal(btn.textContent, "OK");
+        assert.equal(alertMessageEl.resources.en.OK, "OK");
       });
     });
 
