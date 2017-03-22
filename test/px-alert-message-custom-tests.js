1b632a0ccb31b366354ed733710348a85e798b98
Martin Wragg <mdwragg@gmail.com>
2017-03-21 22:56:20 -0700

    Can rationalise component via properties

--- test/px-alert-message-custom-tests.js
+++ test/px-alert-message-custom-tests.js
@@ -65,3 +65,33 @@ describe('Multiple language zh-cn', function() {
   });
 
 });
+
+describe('Properties and attributes in sync for a declarative defined element', function(){
+
+  //info isn't be manipulated
+  it('reflects the "type" property', function() {
+    var autoDismissAlert = fixture('PxAutoDismissAlert');
+    assert.equal(autoDismissAlert.type, "information");
+  });
+
+  it('reflects the "title" property', function() {
+    var autoDismissAlert = fixture('PxAutoDismissAlert');
+    assert.equal(autoDismissAlert.messageTitle, "Heads up!");
+  });
+
+  it('reflects the "message" property', function() {
+    var autoDismissAlert = fixture('PxAutoDismissAlert');
+    assert.equal(autoDismissAlert.message, "This definitely needs your attention.");
+  });
+
+  it('reflects the "action" property', function() {
+    var autoDismissAlert = fixture('PxAutoDismissAlert');
+    assert.equal(autoDismissAlert.action, "https://www.gesoftware.com/predix");
+  });
+
+  it('reflects the "auto-dismiss" property and its a number', function() {
+    var autoDismissAlert = fixture('PxAutoDismissAlert');
+    assert.equal(autoDismissAlert.autoDismiss, 1500);
+  });
+
+});
