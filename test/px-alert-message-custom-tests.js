ee465ba69f79815ee7b6b66f735fa5fa6e3ec0b2
Martin Wragg <mdwragg@gmail.com>
2017-03-21 17:23:35 -0700

    Add Chinese test, fix ‘open’ test

--- test/px-alert-message-custom-tests.js
+++ test/px-alert-message-custom-tests.js
@@ -1,38 +1,52 @@
 describe('Alert action buttons update correctly', function() {
   //Button displays
-    it('action button reflects the "action" property  open', function(done) {
-      var autoDismissAlertEl = fixture('PxDismissAlertMessage');
-      flush(function(){
-        var btn = autoDismissAlertEl.querySelector('.action button');
-        assert.equal(btn.textContent, "Open");
-        done();
-      });
-    });
+  it('action section does not exist', function() {
+    var alertMessageEl = fixture('PxBasicAlertMessage');
+    var actionBtn = alertMessageEl.querySelector('.action');
+    expect(actionBtn).to.not.exist;
+  });
 
-    it('action section does not exist', function() {
-      var alertMessageEl = fixture('PxBasicAlertMessage');
-      var actionBtn = alertMessageEl.querySelector('.action');
-      expect(actionBtn).to.not.exist;
+  it('action button reflects the "dismiss" property', function(done) {
+    var alertMessageEl = fixture('PxBasicAlertMessage');
+    alertMessageEl.action = "dismiss";
+    flush(function(){
+      var icon = alertMessageEl.querySelector('.action .dismiss > iron-icon');
+      expect(icon).to.exist;
+      done();
     });
+  });
 
-    it('action button reflects the "dismiss" property', function(done) {
-      var alertMessageEl = fixture('PxBasicAlertMessage');
-      alertMessageEl.action = "dismiss";
-      flush(function(){
-        var icon = alertMessageEl.querySelector('.action .dismiss > iron-icon');
-        expect(icon).to.exist;
-        done();
-      });
+  it('action button reflects the "action" property  ok', function() {
+    var alertMessageEl = fixture('PxBasicAlertMessage');
+    alertMessageEl.action = "acknowledge";
+    flush(function(){
+      var btn = alertMessageEl.querySelector('.action button');
+      assert.equal(btn.textContent, "OK");
+      assert.equal(alertMessageEl.resources.en.OK, "OK");
     });
+  });
 
-    it('action button reflects the "action" property  ok', function() {
-      var alertMessageEl = fixture('PxBasicAlertMessage');
-      alertMessageEl.action = "acknowledge";
-      flush(function(){
-        var btn = alertMessageEl.querySelector('.action button');
-        assert.equal(btn.textContent, "OK");
-        assert.equal(alertMessageEl.resources.en.OK, "OK");
-      });
+  it('action button reflects the "action" property  open', function(done) {
+    var alertMessageEl = fixture('PxBasicAlertMessage');
+    alertMessageEl.action = "https://www.predix.io/";
+    flush(function(){
+      var btn = alertMessageEl.querySelector('.action button');
+      assert.equal(btn.textContent, "Open");
+      done();
     });
+  });
+
+});
+
+// localisation tests
+describe('Multiple language zh-cn', function() {
+
+  it('if language="zh-cn" ', function() {
+    var multipleLanguageAlert = fixture('PxMultipleLanguageAlert');
+    flush(function(){
+      var moreBtnMessage = multipleLanguageAlert.querySelector('#showMoreButton');
+      assert.equal(moreBtnMessage.textContent, "显示更多");
+    });
+  });
 
 });
