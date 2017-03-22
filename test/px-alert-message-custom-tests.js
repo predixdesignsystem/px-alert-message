7c622a90f8aac796c62d03db31f6ba45d0e3e93f
Martin Wragg <mdwragg@gmail.com>
2017-03-21 17:52:23 -0700

    Try again for localisation tests on sauce.

--- test/px-alert-message-custom-tests.js
+++ test/px-alert-message-custom-tests.js
@@ -19,21 +19,30 @@ describe('Alert action buttons update correctly', function() {
   it('action button reflects the "action" property  ok', function() {
     var alertMessageEl = fixture('PxBasicAlertMessage');
     alertMessageEl.action = "acknowledge";
-    flush(function(){
-      var btn = alertMessageEl.querySelector('.action button');
-      assert.equal(btn.textContent, "OK");
-      assert.equal(alertMessageEl.resources.en.OK, "OK");
+
+    alertMessageEl.addEventListener('app-localize-resources-loaded', function() {
+      flush(function(){
+        var btn = alertMessageEl.querySelector('.action button');
+        assert.equal(btn.textContent, "OK");
+        assert.equal(alertMessageEl.resources.en.OK, "OK");
+        done();
+      });
     });
+    alertMessageEl.loadResources('../locales.json');
   });
 
-  it('action button reflects the "action" property  open', function(done) {
+  it('action button reflects the "action" property  open', function() {
     var alertMessageEl = fixture('PxBasicAlertMessage');
     alertMessageEl.action = "https://www.predix.io/";
-    flush(function(){
-      var btn = alertMessageEl.querySelector('.action button');
-      assert.equal(btn.textContent, "Open");
-      done();
+
+    alertMessageEl.addEventListener('app-localize-resources-loaded', function() {
+      flush(function(){
+        var btn = alertMessageEl.querySelector('.action button');
+        assert.equal(btn.textContent, "Open");
+        done();
+      });
     });
+    alertMessageEl.loadResources('../locales.json');
   });
 
 });
@@ -42,11 +51,17 @@ describe('Alert action buttons update correctly', function() {
 describe('Multiple language zh-cn', function() {
 
   it('if language="zh-cn" ', function() {
+
     var multipleLanguageAlert = fixture('PxMultipleLanguageAlert');
-    flush(function(){
+
+    multipleLanguageAlert.addEventListener('app-localize-resources-loaded', function() {
       var moreBtnMessage = multipleLanguageAlert.querySelector('#showMoreButton');
       assert.equal(moreBtnMessage.textContent, "显示更多");
+      done();
     });
+
+    multipleLanguageAlert.loadResources('../locales.json');
+
   });
 
 });
